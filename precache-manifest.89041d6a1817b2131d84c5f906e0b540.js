8504d4796abc4d5bdb4a36f6f27be7325496d070
Nazar <Nazarsentchak11@gmail.com>
2020-03-13 18:29:27 +0200

    Updates

---
+++ precache-manifest.89041d6a1817b2131d84c5f906e0b540.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd2b220a0276c45cbfd06b4c3a2a9118",
+    "url": "/react-dataTable/index.html"
+  },
+  {
+    "revision": "dddf9737d4ce1d8d321d",
+    "url": "/react-dataTable/static/css/main.d81609e4.chunk.css"
+  },
+  {
+    "revision": "6edca616d0282c40b896",
+    "url": "/react-dataTable/static/js/2.c67510e3.chunk.js"
+  },
+  {
+    "revision": "3b978023f04e196f65a6f1b715c0de90",
+    "url": "/react-dataTable/static/js/2.c67510e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dddf9737d4ce1d8d321d",
+    "url": "/react-dataTable/static/js/main.2d092002.chunk.js"
+  },
+  {
+    "revision": "f7fe889361d3a2655974",
+    "url": "/react-dataTable/static/js/runtime-main.7996ee4c.js"
+  }
+]);
